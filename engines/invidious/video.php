a4d2c056c4858f17759a5e52c8c411f8ed929a77
davidovski <david@davidovski.xyz>
2023-12-30 14:12:08 +0000

    try scraping results page

--- engines/invidious/video.php
+++ engines/invidious/video.php
@@ -2,37 +2,60 @@
     class VideoSearch extends EngineRequest {
         protected $instance_url;
         public function get_request_url() {
-            $this->instance_url = $this->opts->invidious_instance_for_video_results;
             $query = urlencode($this->query);
-            return "$this->instance_url/api/v1/search?q=$query";
+            $key = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8";
+            return "https://www.youtube.com/youtubei/v1/search?key=$key";
         }
 
         public function parse_results($response) {
             $results = array();
-            $json_response = json_decode($response, true);
+            $xpath = get_xpath($response);
+            error_log($response);
 
-            foreach ($json_response as $response) {
-                if ($response["type"] == "video") {
-                    $title = $response["title"];
-                    $url = "https://youtube.com/watch?v=" . $response["videoId"];
-                    $uploader = $response["author"];
-                    $views = $response["viewCount"];
-                    $date = $response["publishedText"];
-                    $thumbnail = $this->instance_url . "/vi/" . explode("/vi/" ,$response["videoThumbnails"][4]["url"])[1];
+            if (!$xpath)
+                return $results;
 
-                    array_push($results,
-                        array (
-                            "title" => htmlspecialchars($title),
-                            "url" =>  htmlspecialchars($url),
-                            // base_url is to be removed in the future, see #47
-                            "base_url" => htmlspecialchars(get_base_url($url)),
-                            "uploader" => htmlspecialchars($uploader),
-                            "views" => htmlspecialchars($views),
-                            "date" => htmlspecialchars($date),
-                            "thumbnail" => htmlspecialchars($thumbnail)
-                        )
-                    );
-                }
+            $results = array();
+
+            foreach($xpath->query("//ytd-video-renderer") as $result) {
+                $url = $xpath->evaluate(".//div//div//div//div//h3//a[@id='results']//@href", $result)[0];
+
+                if ($url == null)
+                    continue;
+
+                $url = $url->textContent;
+
+
+                if (!empty($results) && array_key_exists("url", $results) && end($results)["url"] == $url->textContent)
+                    continue;
+
+                $title = $xpath->evaluate(".//div//div//div//div//h3//a[@id='results']", $result)[0];
+
+                if ($title == null)
+                    continue;
+
+                $title = $title->textContent;
+
+
+                $uploader = $xpath->evaluate(".//div//div//div[@id='channel_info']//ytd-channel-name[@id='channel-name']", $result)[0]->textContent;
+
+                $metadata = $xpath->evaluate(".//div//div//div//ytd-video-meta-block//div[@id='metadata']//div[@id='metadata-line]//span", $result);
+
+                $views = $metadata[0];
+                $date = $metadata[1];
+
+                array_push($results,
+                    array (
+                        "title" => htmlspecialchars($title),
+                        "url" =>  htmlspecialchars($url),
+                        // base_url is to be removed in the future, see #47
+                        "base_url" => htmlspecialchars(get_base_url($url)),
+                        "uploader" => htmlspecialchars($uploader),
+                        "views" => htmlspecialchars($views),
+                        "date" => htmlspecialchars($date),
+                        "thumbnail" => htmlspecialchars($thumbnail)
+                    )
+                );
             }
 
             return $results;
