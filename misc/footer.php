1bc030ca1c2c08e86f1e8e056d48cd39bdd6b022
davidovski <david@davidovski.xyz>
2024-05-04 13:42:01 +0100

    remove commit from footer

--- misc/footer.php
+++ misc/footer.php
@@ -12,7 +12,7 @@
           $hash = file_get_contents(".git/refs/heads/main");
         }
 
-        echo "<a href='https://github.com/Ahwxorg/Librey/commit/$hash' target='_blank'>" . printftext("latest_commit", $hash) . "</a>";
+        //echo "<a href='https://github.com/Ahwxorg/Librey/commit/$hash' target='_blank'>" . printftext("latest_commit", $hash) . "</a>";
     ?>
 </div>
 </body>
